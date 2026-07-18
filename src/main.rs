109b9d3c1256433de0d292e7de8ab2ec7c3c0dd7
Jackelyn <jackelyngironvillacinda@gmail.com>
2026-07-17 18:06:08 -0600

    Dibujo de los poligonos laboratorio

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,35 @@ fn main() {
 
     framebuffer.set_current_color(0xFF00FF);
 
-    let J = [
-        (200,100),(600,100),(600,150),(480,150),(480,450),(160,450),(160,300),(260,300),(260,380),(380,380),(350,150),(200,150)
+    let poligono1 = [
+        (165, 380), (185, 360), (180, 330), (207, 345), (233, 330), (230, 360), (250, 380), (220, 385), (205, 410), (193, 383)
     ];
 
-    framebuffer.draw_polygon(&J);
+    let poligono2 = [
+        (321, 335), (288, 286), (339, 251), (374, 302)
+    ];
 
-    let _ = framebuffer.render_buffer("output.bmp");
+    let poligono3 = [
+        (377, 249), (411, 197), (436, 249)
+    ];
 
-    println!("Framebuffer rendered to output.bmp");
+    let poligono4 = [
+        (413, 177), (448, 159), (502, 88), (553, 53), (535, 36), (676, 37), (660, 52),
+        (750, 145), (761, 179), (672, 192), (659, 214), (615, 214), (632, 230), (580, 230),
+        (597, 215), (552, 214), (517, 144), (466, 180)
+    ];
+
+    let poligono5 = [
+        (682, 175), (708, 120), (735, 148), (739, 170)
+    ];
+
+    framebuffer.draw_polygon(&poligono1);
+    framebuffer.draw_polygon(&poligono2);
+    framebuffer.draw_polygon(&poligono3);
+    framebuffer.draw_polygon(&poligono4);
+    framebuffer.draw_polygon(&poligono5);
+
+    let _ = framebuffer.render_buffer("output.png");
+
+    println!("Framebuffer rendered to output.png");
 } 
